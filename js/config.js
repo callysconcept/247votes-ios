2e70de6f27786076138098de39524c049c7ac891
callysconcept <55920474+callysconcept@users.noreply.github.com>
2021-01-22 22:14:53 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -35,3 +35,13 @@ $(document).ready(function() {
   })
   // Your main code
 })
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+    document.addEventListener("backbutton", function(e){
+           e.preventDefault();
+           navigator.app.exitApp();
+    }, false);
+}
